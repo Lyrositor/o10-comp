e5afa73eac83dfe6f0ad2b4bd9c6d3612280654b
Charles Samborski <demurgos@demurgos.net>
2017-03-12 19:17:42 +0100

    Use shared pointer for type of parameter and variable declaration in the AST

--- include/comp/ast/parameter.h
+++ include/comp/ast/parameter.h
@@ -7,10 +7,10 @@
 namespace comp {
 namespace ast {
 struct Parameter final : public Node {
-  Parameter(Type type, Identifier identifier);
+  Parameter(std::shared_ptr<Type> type, Identifier identifier);
   virtual ~Parameter();
 
-  const Type type;
+  const std::shared_ptr<Type> type;
   const std::shared_ptr<Identifier> identifier;
 };
 }
--- include/comp/ast/variable_declaration.h
+++ include/comp/ast/variable_declaration.h
@@ -9,13 +9,13 @@ namespace comp {
 namespace ast {
 struct VariableDeclaration final : public Declaration {
   VariableDeclaration(
-    Type type,
+    std::shared_ptr<Type> type,
     std::vector<std::shared_ptr<VariableDeclarator>> declarators,
     std::shared_ptr<SourceLocation> location = nullptr
   );
   virtual ~VariableDeclaration();
 
-  const Type type;
+  const std::shared_ptr<Type> type;
   const std::vector<std::shared_ptr<VariableDeclarator>> declarators;
 };
 }
