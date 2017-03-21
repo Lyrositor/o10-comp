c00a0b58c6969d4e58616173211500b4b9e7d47c
antonrautio <anton.rautio@insa-lyon.fr>
2017-03-21 17:59:39 +0100

    Command line interface which takes one argument as a filepath. Handles file not found and permission errors.
    Example:
    $ ./build/comp_main build/test/test.txt

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,9 +2,35 @@
 #include <iostream>
 #include <comp/eval.h>
 
-int main(void) {
-  std::shared_ptr<comp::ast::RExpression> expression = comp::parser::parse("123+456");
-  int64_t value = comp::eval::eval_expression(*expression);
-  std::cout << value << "\n";
+
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv) {
+  int argCount = 0;
+  for (int i = 0; argv[i] != nullptr; i++){
+    argCount++;
+  }
+
+  if (argCount != 2){
+    std::cout << "Incorrect arguments." << std::endl;
+  }
+  else{
+
+    std::ifstream ifs(argv[1]);
+    if (ifs.fail()){
+      std::cerr << strerror(errno) << std::endl;
+    }
+    else{
+      std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                           (std::istreambuf_iterator<char>()    ) );
+      std::shared_ptr<comp::ast::RExpression> expression = comp::parser::parse(content);
+
+      int64_t value = comp::eval::eval_expression(*expression);
+
+      std::cout << value << "\n";
+    }
+  }
+
   return 0;
 }
