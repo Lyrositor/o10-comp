f04866837a06407ee11752b76c17b882d01beb86
antonrautio <anton.rautio@insa-lyon.fr>
2017-03-21 19:22:49 +0100

    Removed unnecessary argCount. Updated to Google style.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,30 +7,19 @@
 #include <fstream>
 
 int main(int argc, char** argv) {
-  int argCount = 0;
-  for (int i = 0; argv[i] != nullptr; i++){
-    argCount++;
-  }
-
-  if (argCount != 2){
+  if (argc != 2) {
     std::cout << "Incorrect arguments." << std::endl;
-  }
-  else{
-
+  } else {
     std::ifstream ifs(argv[1]);
-    if (ifs.fail()){
+    if (ifs.fail()) {
       std::cerr << strerror(errno) << std::endl;
-    }
-    else{
+    } else {
       std::string content( (std::istreambuf_iterator<char>(ifs) ),
                            (std::istreambuf_iterator<char>()    ) );
       std::shared_ptr<comp::ast::RExpression> expression = comp::parser::parse(content);
-
       int64_t value = comp::eval::eval_expression(*expression);
-
       std::cout << value << "\n";
     }
   }
-
   return 0;
 }
