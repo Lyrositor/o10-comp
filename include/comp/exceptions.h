6b2e013cecaeec16146c70504da2e58c8296e901
antonrautio <anton.rautio@insa-lyon.fr>
2017-04-03 11:39:09 +0200

    Create exception classes UnexpectedNodeType, UnexpectedNodeValue, MismatchedTypesLeftRight, FunctionAlreadyDefined, FunctionAlreadyDeclared, FunctionParameterListDoesNotMatch, FunctionReturnTypeDoesNotMatch, FunctionParameterTypeDoesNotMatch in exceptions.h and exceptions.cpp

--- include/comp/exceptions.h
+++ include/comp/exceptions.h
@@ -43,4 +43,91 @@ class UnexpectedTokenError : public SyntaxException {
  private:
   const std::string token_;
 };
+
+class UnexpectedNodeType : public CompilationException {
+ public:
+  UnexpectedNodeType(
+    const std::string &node,
+    const std::shared_ptr<ast::SourceLocation> location);
+
+  std::string GetNode() const;
+
+ private:
+  const std::string node_;
+};
+
+class UnexpectedNodeValue : public CompilationException {
+ public:
+  UnexpectedNodeValue(
+    const std::string &value,
+    const std::string &node,
+    const std::shared_ptr<ast::SourceLocation> location);
+
+  std::string GetNode() const;
+  std::string GetValue() const;
+
+ private:
+  const std::string node_;
+  const std::string value_;
+};
+
+class MismatchedTypesLeftRight : public CompilationException {
+ public:
+  MismatchedTypesLeftRight(
+      const std::string &left,
+      const std::string &right,
+      const std::shared_ptr<ast::SourceLocation> location);
+
+  std::string GetLeft() const;
+  std::string GetRight() const;
+
+ private:
+  const std::string left_;
+  const std::string right_;
+};
+
+class FunctionAlreadyDefined : public CompilationException {
+ public:
+  FunctionAlreadyDefined(
+    const std::string &function,
+    const std::shared_ptr<ast::SourceLocation> location);
+ private:
+  const std::string function_;
+};
+
+class FunctionAlreadyDeclared : public CompilationException {
+ public:
+  FunctionAlreadyDeclared(
+    const std::string &function,
+    const std::shared_ptr<ast::SourceLocation> location);
+ private:
+  const std::string function_;
+};
+
+class FunctionParameterListDoesNotMatch : public CompilationException {
+ public:
+  FunctionParameterListDoesNotMatch(
+    const std::string &function,
+    const std::shared_ptr<ast::SourceLocation> location);
+ private:
+  const std::string function_;
+};
+
+class FunctionReturnTypeDoesNotMatch : public CompilationException {
+ public:
+  FunctionReturnTypeDoesNotMatch(
+    const std::string &function,
+    const std::shared_ptr<ast::SourceLocation> location);
+ private:
+  const std::string function_;
+};
+
+class FunctionParameterTypeDoesNotMatch : public CompilationException {
+ public:
+  FunctionParameterTypeDoesNotMatch(
+      const std::string &function,
+      const std::shared_ptr<ast::SourceLocation> location);
+ private:
+  const std::string function_;
+};
 }  // namespace comp
--- src/lib/exceptions.cpp
+++ src/lib/exceptions.cpp
@@ -36,4 +36,80 @@ UnexpectedTokenError::UnexpectedTokenError(
 std::string UnexpectedTokenError::GetToken() const {
   return token_;
 }
+
+UnexpectedNodeType::UnexpectedNodeType(
+  const std::string &node,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Unexpected node type") + ": '" + node, location),
+    node_(node) {
+}
+
+UnexpectedNodeValue::UnexpectedNodeValue(
+  const std::string &value,
+  const std::string &node,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Unexpected node value") + ": '" + node + " '" + value, location),
+    value_(value),
+    node_(node) {
+}
+
+MismatchedTypesLeftRight::MismatchedTypesLeftRight(
+  const std::string &left,
+  const std::string &right,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Mismatched types for left and right operands") + ": '" + left + ", " + right, location),
+    left_(left),
+    right_(right) {
+}
+
+FunctionAlreadyDefined::FunctionAlreadyDefined(
+  const std::string &function,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Function already defined") + ": '" + function, location),
+    function_(function) {
+}
+
+FunctionAlreadyDeclared::FunctionAlreadyDeclared(
+  const std::string &function,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Function already declared") + ": '" + function, location),
+    function_(function) {
+}
+
+FunctionParameterListDoesNotMatch::FunctionParameterListDoesNotMatch(
+  const std::string &function,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Function's parameter list does not match the declared list") + ": '" + function, location),
+    function_(function) {
+}
+
+FunctionReturnTypeDoesNotMatch::FunctionReturnTypeDoesNotMatch(
+  const std::string &function,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Function's return type does not match the declared type") + ": '" + function, location),
+    function_(function) {
+}
+
+FunctionParameterTypeDoesNotMatch::FunctionParameterTypeDoesNotMatch(
+  const std::string &function,
+  const std::shared_ptr<ast::SourceLocation> location
+) :
+  CompilationException(
+    std::string("Function's parameter type does not match the declared type") + ": '" + function, location),
+    function_(function) {
+}
 }  // namespace comp
