4e6e3b1b06f479fc1d3db3a5076745d69b20118e
gaoyiqin95 <yiqin.gao@insa-lyon.fr>
2017-04-05 17:23:33 +0800

    Optimize the display of variable names in dot.

--- include/comp/ir/to_dot.h
+++ include/comp/ir/to_dot.h
@@ -1,17 +1,31 @@
 #pragma once
 
 #include <memory>
+#include <map>
 #include "comp/dot/ast.h"
 #include "comp/ir/program.h"
 
+
 namespace comp {
 namespace ir {
+
+class IdentifiersTable {
+ public:
+  size_t GetFunctionIdentifier(const FunctionSymbol &f);
+  size_t GetVariableIdentifier(const Variable &v);
+  size_t GetBasicBlockIdentifier(const BasicBlock &bb);
+
+ private:
+  size_t counter = 0;
+  std::map<const void*, size_t> identifers_map;
+};
+
 void EmitOp(const Op &node, std::ostream &out);
 void EmitBasicBlock(const BasicBlock &node, std::ostream &out);
 std::string BasicBlockToString(const BasicBlock &node);
 std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlock &node);
 std::vector<std::shared_ptr<dot::ast::Statement>> ControlFlowGraphToDot(const ControlFlowGraph &node);
-std::vector<std::shared_ptr<dot::ast::Statement>> FunctionSymbolToDot(const FunctionSymbol &node);
+std::vector<std::shared_ptr<dot::ast::Statement>> FunctionSymbolToDot(const FunctionSymbol &node,IdentifiersTable &it);
 std::unique_ptr<dot::ast::Graph> ProgramToDot(const Program &node);
 }
 }
--- src/lib/ir/to_dot.cpp
+++ src/lib/ir/to_dot.cpp
@@ -3,27 +3,49 @@
 
 #include <comp/ir/to_dot.h>
 #include <comp/utils.h>
+#include <map>
+#include <vector>
+#include <sstream>
 
 namespace comp {
 namespace ir {
-std::string pointer_to_string(void const *ptr) {
-  std::stringstream id_stream;
-  id_stream << std::setbase(16) << ptr;
-  return id_stream.str();
+size_t IdentifiersTable::GetFunctionIdentifier(const FunctionSymbol &f){
+  const void* fvoid = static_cast<const void*>(&f);
+  if(identifers_map.find(fvoid)==identifers_map.end()) {
+    identifers_map[fvoid] = counter++;
+  }
+  return identifers_map[static_cast<const void*>(fvoid)];
 }
 
-void EmitVariableOperand(const VariableOperand &node, std::ostream &out) {
-  out << "var" << pointer_to_string(node.variable.get());
+size_t IdentifiersTable::GetVariableIdentifier(const Variable &v){
+  const void* vvoid = static_cast<const void*>(&v);
+  if(identifers_map.find(vvoid)==identifers_map.end()) {
+    identifers_map[vvoid] = counter++;
+  }
+  return identifers_map[vvoid];
+}
+
+size_t IdentifiersTable::GetBasicBlockIdentifier(const BasicBlock &bb){
+  const void* bbvoid = static_cast<const void*>(&bb);
+  if(identifers_map.find(bbvoid)==identifers_map.end()) {
+    identifers_map[bbvoid] = counter++;
+  }
+  return identifers_map[bbvoid];
+}
+
+
+void EmitVariableOperand(const VariableOperand &node, std::ostream &out, IdentifiersTable &it) {
+  out << "var" << it.GetVariableIdentifier(*node.variable.get());
 }
 
 void EmitConstantOperand(const ConstantOperand &node, std::ostream &out) {
   out << node.value;
 }
 
-void EmitOperand(const Operand &node, std::ostream &out) {
+void EmitOperand(const Operand &node, std::ostream &out, IdentifiersTable &it) {
   switch (node.operand_type) {
     case Operand::Type::Variable: {
-      EmitVariableOperand(static_cast<const VariableOperand &>(node), out);
+      EmitVariableOperand(static_cast<const VariableOperand &>(node), out, it);
       break;
     }
     case Operand::Type::Constant: {
@@ -60,50 +82,50 @@ std::string BinaryOperatorToString(const BinOp::BinaryOperator &node) {
   }
 }
 
-void EmitBinOp(const BinOp &node, std::ostream &out) {
-  EmitVariableOperand(*node.out, out);
+void EmitBinOp(const BinOp &node, std::ostream &out, IdentifiersTable &it) {
+  EmitVariableOperand(*node.out, out, it);
   out << " := " << BinaryOperatorToString(node.binaryOperator) << "(";
-  EmitOperand(*node.in1, out);
+  EmitOperand(*node.in1, out, it);
   out << ", ";
-  EmitOperand(*node.in2, out);
+  EmitOperand(*node.in2, out, it);
   out << ");";
 }
 
-void EmitCastOp(const CastOp &node, std::ostream &out) {
-  EmitVariableOperand(*node.out, out);
+void EmitCastOp(const CastOp &node, std::ostream &out, IdentifiersTable &it) {
+  EmitVariableOperand(*node.out, out, it);
   out << " := Cast(";
-  EmitOperand(*node.in, out);
+  EmitOperand(*node.in, out, it);
   out << ");";
 }
 
-void EmitCopyOp(const CopyOp &node, std::ostream &out) {
-  EmitVariableOperand(*node.out, out);
+void EmitCopyOp(const CopyOp &node, std::ostream &out, IdentifiersTable &it) {
+  EmitVariableOperand(*node.out, out, it);
   out << " := Copy(";
-  EmitOperand(*node.in, out);
+  EmitOperand(*node.in, out, it);
   out << ");";
 }
 
-void EmitCallOp(const CallOp &node, std::ostream &out) {
-  EmitVariableOperand(*node.out, out);
+void EmitCallOp(const CallOp &node, std::ostream &out, IdentifiersTable &it) {
+  EmitVariableOperand(*node.out, out, it);
   out << " := CallOp(...);";
 }
 
-void EmitNoOp(const NoOp &node, std::ostream &out) {
+void EmitNoOp(const NoOp &node, std::ostream &out, IdentifiersTable &it) {
   UNUSED(node);
   out << "NoOp;";
 }
 
-void EmitReturnOp(const ReturnOp &node, std::ostream &out) {
+void EmitReturnOp(const ReturnOp &node, std::ostream &out, IdentifiersTable &it) {
   out << "Return(";
   if (node.in != nullptr) {
-    EmitOperand(*node.in, out);
+    EmitOperand(*node.in, out, it);
   }
   out << ");";
 }
 
-void EmitTestOp(const TestOp &node, std::ostream &out) {
+void EmitTestOp(const TestOp &node, std::ostream &out, IdentifiersTable &it) {
   out << "Test(";
-  EmitOperand(*node.test, out);
+  EmitOperand(*node.test, out, it);
   out << ");";
 }
 
@@ -118,45 +140,45 @@ std::string UnaryOperatorToString(const UnaryOp::UnaryOperator &node) {
   }
 }
 
-void EmitUnaryOp(const UnaryOp &node, std::ostream &out) {
-  EmitVariableOperand(*node.out, out);
+void EmitUnaryOp(const UnaryOp &node, std::ostream &out, IdentifiersTable &it) {
+  EmitVariableOperand(*node.out, out, it);
   out << " := " << UnaryOperatorToString(node.unaryOperator) << "(";
-  EmitOperand(*node.in1, out);
+  EmitOperand(*node.in1, out,it);
   out << ");";
 }
 
-void EmitOp(const Op &node, std::ostream &out) {
+void EmitOp(const Op &node, std::ostream &out, IdentifiersTable &it) {
   switch (node.op_type) {
     case Op::Type::BinOp: {
-      EmitBinOp(static_cast<const BinOp &>(node), out);
+      EmitBinOp(static_cast<const BinOp &>(node), out, it);
       break;
     }
     case Op::Type::CastOp: {
-      EmitCastOp(static_cast<const CastOp &>(node), out);
+      EmitCastOp(static_cast<const CastOp &>(node), out, it);
       break;
     }
     case Op::Type::CopyOp: {
-      EmitCopyOp(static_cast<const CopyOp &>(node), out);
+      EmitCopyOp(static_cast<const CopyOp &>(node), out, it);
       break;
     }
     case Op::Type::CallOp: {
-      EmitCallOp(static_cast<const CallOp &>(node), out);
+      EmitCallOp(static_cast<const CallOp &>(node), out, it);
       break;
     }
     case Op::Type::NoOp: {
-      EmitNoOp(static_cast<const NoOp &>(node), out);
+      EmitNoOp(static_cast<const NoOp &>(node), out, it);
       break;
     }
     case Op::Type::ReturnOp: {
-      EmitReturnOp(static_cast<const ReturnOp &>(node), out);
+      EmitReturnOp(static_cast<const ReturnOp &>(node), out, it);
       break;
     }
     case Op::Type::TestOp: {
-      EmitTestOp(static_cast<const TestOp &>(node), out);
+      EmitTestOp(static_cast<const TestOp &>(node), out, it);
       break;
     }
     case Op::Type::UnaryOp: {
-      EmitUnaryOp(static_cast<const UnaryOp &>(node), out);
+      EmitUnaryOp(static_cast<const UnaryOp &>(node), out, it);
       break;
     }
     default: {
@@ -165,7 +187,7 @@ void EmitOp(const Op &node, std::ostream &out) {
   }
 }
 
-void EmitBasicBlock(const BasicBlock &node, std::ostream &out) {
+void EmitBasicBlock(const BasicBlock &node, std::ostream &out,IdentifiersTable &it) {
   std::vector<std::shared_ptr<Op>> ops = node.GetOps();
   if (ops.size() == 0) {
     out << ";";
@@ -174,25 +196,27 @@ void EmitBasicBlock(const BasicBlock &node, std::ostream &out) {
       if (i > 0) {
         out << "\n";
       }
-      EmitOp(*ops[i], out);
+      EmitOp(*ops[i], out, it);
     }
   }
 }
 
-std::string BasicBlockToString(const BasicBlock &node) {
+std::string BasicBlockToString(const BasicBlock &node,IdentifiersTable &it) {
   std::stringstream result;
-  EmitBasicBlock(node, result);
+  EmitBasicBlock(node, result,it);
   return result.str();
 }
 
-std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlock &node) {
+std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlock &node,IdentifiersTable &it) {
   std::vector<std::shared_ptr<dot::ast::Assignment>> attributes;
 
-  std::string ops = BasicBlockToString(node);
+  std::string ops = BasicBlockToString(node,it);
   attributes.push_back(dot::ast::Assignment::Create("label", ops));
   attributes.push_back(dot::ast::Assignment::Create("shape", "rectangle"));
 
-  std::string id = "bb" + pointer_to_string(&node);
+  std::stringstream ss;
+  ss << "bb" << it.GetBasicBlockIdentifier(node);
+  std::string id = ss.str() ;
   std::vector<std::shared_ptr<dot::ast::Statement>> result;
 
   switch (node.GetType()) {
@@ -208,7 +232,10 @@ std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlo
     }
     case BasicBlock::Type::Jump: {
       result.push_back(dot::ast::NodeStatement::Create(id, attributes));
-      result.push_back(dot::ast::EdgeStatement::Create({id, "bb" + pointer_to_string(node.GetBranch().get())}, {}));
+      std::stringstream ss;
+      ss << "bb" << it.GetBasicBlockIdentifier(*node.GetBranch().get());
+      std::string stringss = ss.str() ;
+      result.push_back(dot::ast::EdgeStatement::Create({id, stringss }, {}));
       break;
     }
     case BasicBlock::Type::ConditionalJump: {
@@ -216,14 +243,20 @@ std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlo
       std::shared_ptr<BasicBlock> branch_if_true = node.GetBranchIfTrue();
       std::shared_ptr<BasicBlock> branch_if_false = node.GetBranchIfFalse();
       std::vector<std::string> false_edge;
+      std::stringstream ss;
+      ss << "bb" << it.GetBasicBlockIdentifier(*branch_if_true.get());
+      std::string stringss = ss.str() ;
       result.push_back(
         dot::ast::EdgeStatement::Create(
-          {id, "bb" + pointer_to_string(branch_if_true.get())},
+          {id, stringss},
           {dot::ast::Assignment::Create("color", "#45a03b")})
       );
+      std::stringstream ss2;
+      ss2 << "bb" << it.GetBasicBlockIdentifier(*branch_if_false.get());
+      std::string stringss2 = ss2.str() ;
       result.push_back(
         dot::ast::EdgeStatement::Create(
-          {id, "bb" + pointer_to_string(branch_if_false.get())},
+          {id, stringss2},
           {dot::ast::Assignment::Create("color", "#a52f29")})
       );
       break;
@@ -241,21 +274,21 @@ std::vector<std::shared_ptr<dot::ast::Statement>> BasicBlockToDot(const BasicBlo
   return result;
 }
 
-std::vector<std::shared_ptr<dot::ast::Statement>> ControlFlowGraphToDot(const ControlFlowGraph &node) {
+std::vector<std::shared_ptr<dot::ast::Statement>> ControlFlowGraphToDot(const ControlFlowGraph &node,IdentifiersTable &it) {
   std::vector<std::shared_ptr<dot::ast::Statement>> result;
   std::set<std::shared_ptr<BasicBlock>> basic_blocks = node.GetBasicBlocks();
   for (auto basic_block : basic_blocks) {
-    for (auto statement : BasicBlockToDot(*basic_block)) {
+    for (auto statement : BasicBlockToDot(*basic_block,it)) {
       result.push_back(statement);
     }
   }
   return result;
 }
 
-std::vector<std::shared_ptr<dot::ast::Statement>> FunctionSymbolToDot(const FunctionSymbol &node) {
+std::vector<std::shared_ptr<dot::ast::Statement>> FunctionSymbolToDot(const FunctionSymbol &node, IdentifiersTable &it) {
   std::vector<std::shared_ptr<dot::ast::Statement>> body;
 
-  std::vector<std::shared_ptr<dot::ast::Statement>> cfg = ControlFlowGraphToDot(*node.GetBody());
+  std::vector<std::shared_ptr<dot::ast::Statement>> cfg = ControlFlowGraphToDot(*node.GetBody(),it);
   for (auto statement : cfg) {
     body.push_back(statement);
   }
@@ -264,19 +297,22 @@ std::vector<std::shared_ptr<dot::ast::Statement>> FunctionSymbolToDot(const Func
   body.push_back(dot::ast::AssignmentStatement::Create("label", name + " @Function"));
 
   std::vector<std::shared_ptr<dot::ast::Statement>> result;
-  std::string id = "cluster" + pointer_to_string(&node);
+  std::stringstream ss;
+  ss << "cluster" << it.GetFunctionIdentifier(node);
+  std::string id = ss.str() ;
   result.push_back(dot::ast::SubgraphStatement::Create(std::shared_ptr<std::string>(new std::string(id)), body));
   return result;
 }
 
 std::unique_ptr<dot::ast::Graph> ProgramToDot(const Program &node) {
   std::vector<std::shared_ptr<dot::ast::Statement>> body;
+  IdentifiersTable identifiersTable;
 
   for (auto symbol: node.GetSymbols()) {
     std::vector<std::shared_ptr<dot::ast::Statement>> statements;
     switch (symbol->GetType()) {
       case ProgramSymbol::Type::Function: {
-        statements = FunctionSymbolToDot(static_cast<const FunctionSymbol &>(*symbol));
+        statements = FunctionSymbolToDot(static_cast<const FunctionSymbol &>(*symbol),identifiersTable);
         break;
       }
       case ProgramSymbol::Type::DataObject: {
