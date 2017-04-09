9aea4253a6b3689fe76ccfa1cbd5fd6a966b42e0
Lyrositor <gagne.marc@gmail.com>
2017-04-09 12:55:01 +0200

    Add a test for arrays

---
+++ end-to-end/programs/arrays/main.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void main() {
+  char array[10];
+  array[0] = '0';
+  array[1] = '1';
+  array[2] = '2';
+  array[3] = '3';
+  array[4] = '4';
+  array[5] = '5';
+  array[6] = '6';
+  array[7] = '7';
+  array[8] = '8';
+  array[9] = '9';
+  putchar(array[0]);
+  putchar(array[1]);
+  putchar(array[2]);
+  putchar(array[3]);
+  putchar(array[4]);
+  putchar(array[5]);
+  putchar(array[6]);
+  putchar(array[7]);
+  putchar(array[8]);
+  putchar(array[9]);
+  putchar('\n');
+}
